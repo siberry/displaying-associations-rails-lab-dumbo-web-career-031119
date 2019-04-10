e84d2b184f268508eb532e837d2fbfb883c784b7
siberry <jwy5140@gmail.com>
2019-04-10 10:37:43 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    self.artist.name
+  end
+
+  def artist_song
+    "#{self.artist.name} - #{self.title}"
+  end
+
+
+
 end
